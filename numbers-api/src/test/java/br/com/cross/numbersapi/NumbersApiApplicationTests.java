45323f9fc2a89f67bb5ad2c9da21f7ed9c9d6c01
gustavodiniz <gustavo.diniz@tangerino.com.br>
2021-12-03 21:34:31 -0300

    created project numbers-api

---
+++ numbers-api/src/test/java/br/com/cross/numbersapi/NumbersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cross.numbersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
